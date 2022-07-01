40ba2666f5415acfc22a980df1ed905b01dfe00c
retharikc <retoju.harika@capgemini.com>
2022-07-01 21:13:18 +0530

    jenkins

---
+++ src/main/java/com/example/demo/JenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+	public static Logger logger= LoggerFactory.getLogger(JenkinsApplication.class);
+	
+	public void intt()
+	{
+		logger.info("Application started...");
+	}
+	
+	public static void main(String[] args) {
+		
+		logger.info("Application executed..");
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+}
+
+
